57415edc7935ad57d086365ce2ff22ced3aac844
Kurt Bauer <kurtbauer55@protonmail.com>
2018-04-10 19:17:08 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,12 +15,11 @@ def speak_to_grandma(words)
 
 if words == "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
+elsif words.upcase
+  return "NO, NOT SINCE 1938!"
 else
   return "HUH?! SPEAK UP, SONNY!"
 end
 
-if words.upcase
-  return "NO, NOT SINCE 1938!"
-end
 
 end
