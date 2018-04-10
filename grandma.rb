8688a39f4946154c1285d22040f8484fa6f47b40
Kurt Bauer <kurtbauer55@protonmail.com>
2018-04-10 20:29:04 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -24,7 +24,7 @@ if words == "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
 elsif words == words.upcase
   return "NO, NOT SINCE 1938!"
-elsif words == words.downcase
+else
   return "HUH?! SPEAK UP, SONNY!"
 end
 
