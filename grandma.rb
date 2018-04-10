60a5976d4cdce6b684fa0a6b7b0e0f807b458b27
Kurt Bauer <kurtbauer55@protonmail.com>
2018-04-10 18:29:30 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -17,10 +17,11 @@ if words == "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
 else
   return "HUH?! SPEAK UP, SONNY!"
-end
-
-if words
+else
+  return "NO, NOT SINCE 1938!"
+else
   return "NO, NOT SINCE 1938!"
 end
+
 
 end
